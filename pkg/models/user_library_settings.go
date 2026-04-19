695bc6b8198322c5fd60101779ed24a20daf1609
Robin Joseph <robinmjoseph08@gmail.com>
2026-04-19 12:37:34 -0500

    [Backend] Add UserLibrarySettings model

---
+++ pkg/models/user_library_settings.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"github.com/uptrace/bun"
+)
+
+type UserLibrarySettings struct {
+	bun.BaseModel `bun:"table:user_library_settings,alias:uls"`
+
+	ID        int       `bun:",pk,autoincrement"                            json:"id"`
+	CreatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp" json:"created_at"`
+	UpdatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp" json:"updated_at"`
+	UserID    int       `bun:",notnull"                                     json:"user_id"`
+	LibraryID int       `bun:",notnull"                                     json:"library_id"`
+	SortSpec  *string   `bun:",nullzero"                                    json:"sort_spec"`
+}
